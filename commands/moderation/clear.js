59203d446b8b29e6da32a1b9fb65aec370823c21
roteKlaue <janoolhd@gmail.com>
2022-12-14 15:44:52 +0100

    made clear command reply ephemeral:

--- commands/moderation/clear.js
+++ commands/moderation/clear.js
@@ -35,7 +35,7 @@ module.exports = {
         if (deletedMessagesCount === 0) 
             return message.reply("I can't delete messages which are older than two weeks.");
 
-        message.reply(`Deleted ${deletedMessagesCount} messages from <#${message.channel.id}>`).then(msg => {
+        message.reply({ content:`Deleted ${deletedMessagesCount} messages from <#${message.channel.id}>`, ephemeral:true }).then(msg => {
             setTimeout(() => {try{msg.delete()} catch(e) {}}, 5000);
         });
     }
